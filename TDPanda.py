ba71bc349da053adc7606f0105e5cf371db29328
bigbac64 <50989364+bigbac64@users.noreply.github.com>
2021-03-16 17:06:37 +0100

    Update TDPanda.py

--- TDPanda.py
+++ TDPanda.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[21]:
 
 
 import pandas as pd
+
+
+# In[22]:
+
+
+import numpy as np
 
 
 # In[2]:
@@ -17,6 +23,36 @@ csv = pd.read_csv('mower_market_snapshot.csv',delimiter=";")
 
 
 csv.head()
+
+
+# In[28]:
+
+
+csv["prod_cost"]
+
+
+# In[26]:
+
+
+csv[csv["prod_cost"].isin(["unknown"])]
+
+
+# In[32]:
+
+
+csv.groupby(["warranty"]).first()
+
+
+# In[49]:
+
+
+csv["warranty"] = csv["warranty"].apply(lambda x: x[0])
+
+
+# In[51]:
+
+
+csv.groupby(["warranty"]).first()
 
 
 # In[ ]:
