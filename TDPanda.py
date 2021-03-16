acf2c4a1c2962bafb1ec3c802dde0a90438e013e
bigbac64 <50989364+bigbac64@users.noreply.github.com>
2021-03-16 16:03:37 +0100

    Create TDPanda.py

---
+++ TDPanda.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+csv = pd.read_csv('mower_market_snapshot.csv',delimiter=";")
+
+
+# In[3]:
+
+
+csv.head()
+
+
+# In[ ]:
+
+
+
+
