eeeee66e19a31b33aa87f5c5a19df4245bdf0629
bigbac64 <50989364+bigbac64@users.noreply.github.com>
2021-03-17 10:31:05 +0100

    Update TDPanda.py

--- TDPanda.py
+++ TDPanda.py
@@ -1,58 +1,89 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[21]:
+# In[1]:
 
 
 import pandas as pd
 
 
-# In[22]:
+# In[2]:
 
 
 import numpy as np
 
 
-# In[2]:
+# In[3]:
 
 
 csv = pd.read_csv('mower_market_snapshot.csv',delimiter=";")
 
 
-# In[3]:
+# In[4]:
 
 
 csv.head()
 
 
-# In[28]:
+# In[5]:
 
 
 csv["prod_cost"]
 
 
-# In[26]:
+# In[6]:
 
 
 csv[csv["prod_cost"].isin(["unknown"])]
 
 
-# In[32]:
+# In[12]:
+
+
+csv.isnull().sum()
+
+
+# In[8]:
 
 
 csv.groupby(["warranty"]).first()
 
 
-# In[49]:
+# In[9]:
 
 
 csv["warranty"] = csv["warranty"].apply(lambda x: x[0])
 
 
-# In[51]:
+# In[10]:
 
 
 csv.groupby(["warranty"]).first()
+
+
+# In[21]:
+
+
+csv.groupby(pd.cut(csv["price"], [0, 300,500,1000])).mean().loc[:,["capacity", "failure_rate", "margin","market_share", "attractiveness"]]
+
+
+# In[23]:
+
+
+csv["product_type"] = csv["product_type"].apply(lambda x: 1 if x=="essence" else 2 if x=="electrique" else 3)
+
+
+# In[25]:
+
+
+csv.groupby(["product_type"]).first()
+
+
+# In[30]:
+
+
+csv = csv.assign(Essence=csv["product_type"].apply(lambda x: 1 if x==1else 0), Electrique=csv["product_type"].apply(lambda x: 1 if x==2 else 0), Auto_portee=csv["product_type"].apply(lambda x: 1 if x==3 else 0))
+csv.groupby(["product_type"]).first()
 
 
 # In[ ]:
